8e51ce15a095f8dc0c887ff0f19fb0622059a2df
Federico Rosales <fede17rosales@gmail.com>
2021-01-23 21:40:59 -0300

    primer modelo de noticias

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NoticiasService } from '../../services/noticias.service';
-import { RespuestaTopHeadLines } from '../../Interfaces/Interfaces';
+import { RespuestaTopHeadLines, Article } from '../../Interfaces/Interfaces';
 
 @Component({
   selector: 'app-tab1',
@@ -10,6 +10,7 @@ import { RespuestaTopHeadLines } from '../../Interfaces/Interfaces';
   // importo el OnInit 
 export class Tab1Page implements OnInit {
 
+  noticias: Article[] = [];
   // importo el servicio de noticias
   constructor(private NoticiasService: NoticiasService ) {}
 
@@ -17,6 +18,7 @@ export class Tab1Page implements OnInit {
     this.NoticiasService.getTopHeadlines()
     .subscribe(res =>{ // subscribe me permite traer la información y tengo que pasarle la resp(de la cual va tener un parametro)
       console.log('noticias', res);
+      this.noticias.push( ...res.articles);
     })
   }
 }
