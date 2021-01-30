ac5f735c1d9cc48fae247f5fa20b7223a68fc8a0
frosales <federico.rosales@cablasociados.com>
2021-01-30 19:50:38 -0300

    agrego parte de segmentos

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  @ViewChild(IonSegment,  {static: true}) segment: IonSegment;
+
+  categorias = ['Negocios', 'Entretenimiento', 'General', 'Salud', 'Ciencia', 'Deportes', 'Tecnología'];
+
+  ngOnInit() {
+    this.segment.value = this.categorias[0];
+  }
+
 
 }
--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -9,8 +9,9 @@ export class NoticiasService {
 
   // importo el componente HttpClient de la common
   constructor(private http: HttpClient) { }
-    
+
     getTopHeadlines() {
+      // tslint:disable-next-line: max-line-length
       return this.http.get<RespuestaTopHeadLines>(`https://newsapi.org/v2/top-headlines?country=ar&apiKey=9af975a277674f9789b24b46da5072e2`);
     } // creo la funcion para conseguir la info, <RespuestaTopHeadLines> -> es de tipo observable y la importo desde interface
 
