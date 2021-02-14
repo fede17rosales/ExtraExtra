ae1b25a07dc4bfd35f960d7272ded9c727aaf9ad
Federico Rosales <fede17rosales@gmail.com>
2021-02-14 20:59:57 -0300

    noticias final

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../../Interfaces/Interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { ActionSheetController } from '@ionic/angular';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { DataLocalService } from '../../services/data-local.service';
 
 @Component({
   selector: 'app-noticia',
@@ -12,16 +14,49 @@ export class NoticiaComponent implements OnInit {
 
   @Input() noticia: Article;
   @Input() indice: number;
-  constructor(private iab: InAppBrowser,
-              private actionSheetCtrl: ActionSheetController) { }
+  @Input() enFavoritos;
 
-  ngOnInit() {}
+  constructor(private iab: InAppBrowser,
+              private actionSheetCtrl: ActionSheetController,
+              private socialSharing: SocialSharing,
+              private dataLocal: DataLocalService) { }
+
+  ngOnInit() {
+
+    console.log('Favoritos', this.enFavoritos);
+  }
 
   abrirNoticia() {
     const browser = this.iab.create(this.noticia.url, '_system');
   }
 
   async lanzarMenu(){
+
+    let guardarBorrarbtn;
+
+    if (this.enFavoritos){
+       // borrar de favoritos
+       guardarBorrarbtn = {
+        text: 'Borrar Favorito',
+        icon: 'trash',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Borrar de Favorito');
+          this.dataLocal.borrarNoticia(this.noticia);
+        }
+      };
+    }else {
+      guardarBorrarbtn = {
+        text: ' Favorito',
+        icon: 'star',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Favorito');
+          this.dataLocal.guardarNoticia(this.noticia);
+        }
+      };
+    }
+
     const actionSheet = await this.actionSheetCtrl.create({
       buttons: [ {
         text: 'Compartir',
@@ -29,15 +64,15 @@ export class NoticiaComponent implements OnInit {
         cssClass: 'action-dark',
         handler: () => {
           console.log('Share clicked');
+          this.socialSharing.share(
+            this.noticia.title,
+            this.noticia.source.name,
+            '',
+            this.noticia.url);
         }
-      }, {
-        text: 'Favorito',
-        icon: 'star',
-        cssClass: 'action-dark',
-        handler: () => {
-          console.log('Favorito');
-        }
-      }, {
+      },
+      guardarBorrarbtn
+      , {
         text: 'Cancelar',
         icon: 'close',
         cssClass: 'action-dark',
